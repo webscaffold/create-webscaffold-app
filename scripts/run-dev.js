b5615526315151e02877640513887790c1154c56
Radu Micu <radu.micu@gmail.com>
2019-07-23 15:27:10 +0100

    Update dev tasks, bump deps, add TypeScript check WIP

--- scripts/run-dev.js
+++ scripts/run-dev.js
@@ -5,15 +5,36 @@ const path = require('path');
 const Emittery = require('emittery');
 const { reporter } = require('@webscaffold/task-core');
 const clean = require('@webscaffold/task-clean');
-const copyStatic = require('@webscaffold/task-copy');
+const copy = require('@webscaffold/task-copy');
 const cssCompiler = require('@webscaffold/task-css-compiler').compiler;
 const jsCompiler = require('@webscaffold/task-js-compiler');
 // // const imageResize = require('./tasks/image-resize');
 // const bs = require('./tasks/browser-sync');
 // const runServer = require('./tasks/run-server');
-// const watcher = require('./tasks/watcher');
+const watcher = require('@webscaffold/task-watcher');
 const { config } = require('./config');
 const webpackConfig = require('./config/webpack.config.js');
+
+const eventBus = new Emittery();
+const appDirectory = fs.realpathSync(process.cwd());
+const resolvePath = (relativePath) => path.resolve(appDirectory, relativePath);
+
+const copyStaticTask = () => copy('static/**/*', resolvePath(config.paths.buildPath), {
+	taskName: 'copy:static',
+	cpy: {
+		cwd: resolvePath(config.paths.srcPath),
+		parents: true
+	}
+});
+
+const cssCompilerTask = () => cssCompiler(config.paths.stylesEntryPoint, config.paths.stylesOutputDest, {
+	isDebug: options.isDebug,
+	eventBus,
+	buildPath: resolvePath(config.paths.buildPath),
+	sass: {
+		sourceMapEmbed:	options.isDebug
+	}
+});
 
 /**
  * Run the dev task, compile css and js and run the local server
@@ -21,33 +42,15 @@ const webpackConfig = require('./config/webpack.config.js');
  * @param {object} options - Task options object
  * @returns {Promise} - Promise object
  */
-async function runDev(options) {
-	const eventBus = new Emittery();
-	const appDirectory = fs.realpathSync(process.cwd());
-	const resolvePath = (relativePath) => path.resolve(appDirectory, relativePath);
-
-
+module.exports = async function(options) {
 	reporter('dev').emit('log', 'starting dev');
 
 	await clean([`${resolvePath(config.paths.buildPath)}/*`], { reporter });
 
-	await copyStatic('static/**/*', resolvePath(config.paths.buildPath), {
-		taskName: 'copy:static',
-		cpy: {
-			cwd: resolvePath(config.paths.srcPath),
-			parents: true
-		}
-	});
+	await copyStaticTask();
 
 	await Promise.all([
-		cssCompiler(config.paths.stylesEntryPoint, config.paths.stylesOutputDest, {
-			isDebug: options.isDebug,
-			eventBus,
-			buildPath: resolvePath(config.paths.buildPath),
-			sass: {
-				sourceMapEmbed:	options.isDebug
-			}
-		}),
+		cssCompilerTask(),
 		(async () => {
 			process.env.BROWSERSLIST_ENV = 'modern';
 			await jsCompiler(webpackConfig(config).modernConfig, { eventBus })
@@ -66,20 +69,8 @@ async function runDev(options) {
 	// 	cert: `src/ssl/${process.env.SSL_CERT_FILE_NAME}`
 	// });
 
-	// // TODO: Add it back
-	// // await new watcher(['src/static/**/*.*'], { ...opts, label: 'static assets' }, () => copyStatic(opts));
-	// await new watcher([`${config.paths.srcPath}/styles/**/*.scss`], { label: 'sass files' }, () => {
-	// 	return compileCSS(config.paths.stylesEntryPoint, config.paths.stylesOutputDest, {
-	// 		isDebug: options.isDebug,
-	// 		eventBus,
-	// 		buildPath: config.paths.buildPath,
-	// 		sass: {
-	// 			sourceMapEmbed:	options.isDebug
-	// 		}
-	// 	});
-	// });
+	await new watcher(['src/static/**/*.*'], { label: 'static assets' }, () => copyStaticTask());
+	await new watcher([`${config.paths.srcPath}/styles/**/*.scss`], { label: 'sass files' }, () => cssCompilerTask());
 	// await new watcher(['src/html/**/*.*'], { label: 'html files' }, () => runServer({ inspect: options.nodeInspect }));
 	// await new watcher(['src/server/**/*.js'], { label: 'server files' }, () => runServer({ inspect: options.nodeInspect }));
-}
-
-module.exports = runDev;
+};
