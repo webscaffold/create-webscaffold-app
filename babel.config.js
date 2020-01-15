a3f3a4866ce98ed80ebdcb00a6c13eeefc6d65e4
Radu Micu <radu.micu@gmail.com>
2020-01-15 13:57:14 +0000

    Add @babel/plugin-proposal-nullish-coalescing-operator

--- babel.config.js
+++ babel.config.js
@@ -28,6 +28,7 @@ module.exports = function(api) {
 		'@babel/plugin-syntax-dynamic-import',
 		'@babel/plugin-proposal-class-properties',
 		'@babel/plugin-transform-runtime',
+		'@babel/plugin-proposal-nullish-coalescing-operator',
 		'@babel/plugin-proposal-optional-chaining',
 		'console'
 	];
