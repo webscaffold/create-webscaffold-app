a47d4f78774df0b247e96a2e51610c4cd0022a10
Radu Micu <radu.micu@gmail.com>
2020-01-20 14:52:36 +0000

    feat(babel): remove console babel plugin

--- babel.config.js
+++ babel.config.js
@@ -29,8 +29,7 @@ module.exports = function(api) {
 		'@babel/plugin-proposal-class-properties',
 		'@babel/plugin-transform-runtime',
 		'@babel/plugin-proposal-nullish-coalescing-operator',
-		'@babel/plugin-proposal-optional-chaining',
-		'console'
+		'@babel/plugin-proposal-optional-chaining'
 	];
 
 	return {
